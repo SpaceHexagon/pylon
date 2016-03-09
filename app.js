18b7f7d0019f1f120714f97d28600491c26a4562
FreshDesignza <service@freshdesignza.ca>
2016-03-09 14:34:55 -0500

    Re-enabled displaying of error messages (as json for now)

--- app.js
+++ app.js
@@ -21,15 +21,12 @@ var app = express();
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.set('json spaces', 2);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-//app.get("/", function (req, res) {
-//	res.sendFile('/index.htm');
-//});
 
 app.use('/users', users);
 
@@ -41,16 +38,12 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-//    res.render('error', {
-//      message: err.message,
-//      error: err
-//    });
+    res.json(err);
   });
 }
 
@@ -58,10 +51,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-//  res.render('error', {
-//    message: err.message,
-//    error: {}
-//  });
+  res.json(err);
 });
 
 
