5f8d8b65a4b8cd16c92998b3f8195b2b7a7c1e81
Jeremy Evans <openspacehexagon@gmail.com>
2016-03-10 21:59:48 -0500

    Updated Config

--- app/config.js
+++ app/config.js
@@ -2,8 +2,7 @@ var fs = require('fs');
 
 module.exports = {
 	secret: "",
-	key:    fs.readFileSync('/root/spacehexagon/private-key.key'),
-	cert:   fs.readFileSync('/root/spacehexagon/certificate.crt'),
-	//cert: fs.readFileSync('/etc/ssl/certs/cert-spacehexagon-bundle.crt')
-   ca:     fs.readFileSync('/root/spacehexagon/intermediate-certificate.crt')
+	key:    fs.readFileSync('private-key.key'),
+	cert:   fs.readFileSync('certificate.crt'),
+   	ca:     fs.readFileSync('intermediate-certificate.crt')
 };
