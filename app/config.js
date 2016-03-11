840292c952952aec849076b7c3e61eb07ef47955
Jeremy Evans <openspacehexagon@gmail.com>
2016-03-10 22:08:44 -0500

    Added skeleton structure of client side scripts (UI components, Applets...)

--- app/config.js
+++ app/config.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 
 module.exports = {
 	secret: "",
+	test: "",
 	key:    fs.readFileSync('private-key.key'),
 	cert:   fs.readFileSync('certificate.crt'),
    	ca:     fs.readFileSync('intermediate-certificate.crt')
