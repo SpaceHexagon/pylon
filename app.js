e2cbbf817a7aec9dc4853b6d42d61c0c75d9f0b7
Jeremy Evans <openspacehexagon@gmail.com>
2016-03-19 15:57:14 -0400

    Progress toward files uploads

--- app.js
+++ app.js
@@ -40,8 +40,10 @@ var usersOnline = [];
 app.set('superSecret', config.secret);
 app.set('json spaces', 2);
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/api/files.js
+++ routes/api/files.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-    ObjectID = require('mongodb').ObjectID;
+    ObjectID = require('mongodb').ObjectID,
+	Busboy = require('busboy');
 
 // File Routes
 module.exports = function (app, db) {
@@ -7,11 +8,33 @@ module.exports = function (app, db) {
         Files = db.collection("files");
 
 	router.post('/', function(req, res) {
-		var results = {"uploading file": ""};
-		res.json(results);
+		// Create an Busyboy instance passing the HTTP Request headers.
+		var busboy = new Busboy({ headers: req.headers });
+		// Listen for event when Busboy finds a file to stream.
+		busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
+			// We are streaming! Handle chunks
+			file.on('data', function (data) {
+				// Here we can act on the data chunks streamed.
+			});
+			// Completed streaming the file.
+			file.on('end', function () {
+				console.log('Finished with ' + fieldname);
+			});
+		});
+		// Listen for event when Busboy finds a non-file field.
+		busboy.on('field', function (fieldname, val) {
+			// Do something with non-file field.
+		});
+		// Listen for event when Busboy is finished parsing the form.
+		busboy.on('finish', function () {
+			res.statusCode = 200;
+			res.end();
+		});
+		// Pipe the HTTP Request into Busboy.
+		req.pipe(busboy);
 	});
 
-	router.post('//create/', function(req, res) {
+	router.post('/create/', function(req, res) {
 		Files.insert({name: req.body.name, data: req.body.data}, function(err){
             if (err) {
                 return console.log("Error inserting file: ", err);
