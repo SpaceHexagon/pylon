1275924c58df951e1b94bf416cc54d51fc0dd5d3
Jeremy Evans <openspacehexagon@gmail.com>
2016-05-15 07:25:06 -0400

    Got (still) imagery to broadcast over websockets..

--- src/js/main.js
+++ src/js/main.js
@@ -86,7 +86,7 @@ socket.on('user update', function (userData) {
 						sys = app;
 				if (userData.username != sys.username) {
 					if (sys.users[userData.username] == null) {
-						avatar = new Avatar("default", {username: userData.username, profilePicture: ""});
+						avatar = new Avatar("default", {username: userData.username, profilePicture: userData.image});
 						sys.users[userData.username] = {
 								"user": userData.username,
 								"mesh": avatar.mesh,
@@ -96,6 +96,9 @@ socket.on('user update', function (userData) {
 							user = app.users[userData.username];
 							user.mesh.position.set(userData.position.x, userData.position.y, userData.position.z);
 							user.mesh.quaternion.set(userData.quaternion.x, userData.quaternion.x, userData.quaternion.x, userData.quaternion.w);
+							if (userData.image != "") {
+								user.updateImage(userData.image);
+							}
 							userData.arms.forEach(function (arm, i) {
 								user.arms[i].position.set(arm.pos[0], arm.pos[1], arm.pos[2]);
 								user.arms[i].quaternion.set(arm.quat[0], arm.quat[1], arm.quat[2], arm.quat[3]);
--- src/js/vr/avatar.js
+++ src/js/vr/avatar.js
@@ -10,27 +10,27 @@ export default class Avatar {
 
         if (!! options) {
             if (!! options.profilePicture) { // false ) { //
-							var img = document.createElement('img');
-							img.onload = function (e) {
-								var face = new THREE.Mesh(new THREE.PlaneGeometry(600, 600), new THREE.MeshBasicMaterial({side:2, color: 0xffffff, map: new THREE.Texture(e.target)}));
-								face.material.map.needsUpdate = true;
-								//face.rotateX(Math.PI/2);
-								mesh.add(face);
-							};
-							img.crossOrigin = ''; // no credentials flag. Same as img.crossOrigin='anonymous'
-							img.src = options.profilePicture;
-			            } else {
-							mesh = new THREE.Mesh(
-								new THREE.OctahedronGeometry(300, 0),
-								new THREE.MeshPhongMaterial({color:0xffffff, wireframe: false})
-							);
-							userShield = new THREE.Mesh(
-								new THREE.OctahedronGeometry(333, 1),
-								new THREE.MeshBasicMaterial({wireframe: false, color: 0xffffff,})
-							);
-							mesh.add(userShield);
+								var img = document.createElement('img');
+								img.onload = function (e) {
+									var face = new THREE.Mesh(new THREE.PlaneGeometry(1600, 1600), new THREE.MeshBasicMaterial({side:2, color: 0xffffff, map: new THREE.Texture(e.target)}));
+									face.material.map.needsUpdate = true;
+									mesh.add(face);
+								};
+								img.crossOrigin = ''; // no credentials flag. Same as img.crossOrigin='anonymous'
+								img.src = options.profilePicture;
+						} else {
+								mesh = new THREE.Mesh(
+									new THREE.OctahedronGeometry(1600, 0),
+									new THREE.MeshPhongMaterial({color:0xffffff, wireframe: false})
+								);
+								userShield = new THREE.Mesh(
+									new THREE.OctahedronGeometry(1800, 1),
+									new THREE.MeshBasicMaterial({wireframe: false, color: 0xffffff,})
+								);
+								mesh.add(userShield);
 						}
         }
+
         for (a = 0; a < 2; a++) {
             arm = new THREE.Mesh(new THREE.BoxGeometry(100, 100, 350), new THREE.MeshBasicMaterial({wireframe: true, color:0xffffff}));
             arm.position.set(-150+(a*300), -180, -300);
@@ -44,11 +44,26 @@ export default class Avatar {
 				three.scene.add(mesh);
 
 		 return {
-			name: name,
-			user_id: user_id,
-			data: data,
-			arms: arms,
-			mesh: mesh
+				name: name,
+				user_id: user_id,
+				data: data,
+				arms: arms,
+				mesh: mesh,
+				updateImage: function (image) {
+						var face = mesh.children[0],
+								avatar = mesh;
+
+						avatar.remove(face);
+						var img = document.createElement('img');
+						img.onload = function (e) {
+							var face = new THREE.Mesh(new THREE.PlaneGeometry(600, 600),
+							 													new THREE.MeshBasicMaterial({side:2, color: 0xffffff, map: new THREE.Texture(e.target)}));
+							face.material.map.needsUpdate = true;
+							avatar.add(face);
+						};
+						img.crossOrigin = '';
+						img.src = image;
+				}
 		 }
 	}
 }
