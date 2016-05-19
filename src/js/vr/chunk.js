41561d9f90ee67f77446b2dfa60f673edabc12ee
Jeremy Evans <openspacehexagon@gmail.com>
2016-05-19 00:25:21 -0400

    Adjusted terrain layer

--- src/js/vr/chunk.js
+++ src/js/vr/chunk.js
@@ -5,24 +5,25 @@ import Avatar from './avatar.js';
 export default class Chunk {
 	constructor(coords, mobile, data) {
 		var material = new THREE.MeshBasicMaterial(),
-				geometry = new THREE.Geometry(),
-				cellGeom = new THREE.CylinderGeometry(3400, 3400, 3000, 6),
-				tallCell = new THREE.CylinderGeometry(3400, 3400, 6800, 6),
+				finalGeom = new THREE.Geometry(),
+				cellGeom = new THREE.CylinderGeometry(3500, 3500, 6800, 6),
+				tallCell = new THREE.CylinderGeometry(3400, 3400, 1320, 6),
 				cellSize = 6200,
 				narrow = cellSize*0.9,
 				mesh = null,
+				bsp = null,
 				cellMesh = null,
 				cellWidth = 6,
 				chunkWidth = cellSize * cellWidth,
 				chunkLength = 3200 * 6 * Math.sqrt(3),
 				base = new THREE.Geometry(),
 				baseMaterial = (app.mobile ? new THREE.MeshLambertMaterial({
-					color: (coords[0]+coords[2])%2 == 0 ? 0xafafaf : 0xffffff,
+					color: (coords[0]+coords[2])%3 == 0 ? 0xdfdfdf : 0xffffff,
 					shading: THREE.FlatShading
 				}) : new THREE.MeshPhongMaterial({
-					color: (coords[0]+coords[2])%2 == 0 ? 0xafafaf : 0xffffff,
+					color: (coords[0]+coords[2])%3 == 0 ? 0xdfdfdf : 0xffffff,
 					specular: 0xffffff,
-					shininess: 30,
+					shininess: 20,
 					shading: THREE.FlatShading
 				})),
 				cell = null,
@@ -40,11 +41,17 @@ export default class Chunk {
 				cellMesh.position.set((x*cellSize)+(y % 2==0 ? 0 : cellSize / 2), (altitude * cellSize)/4, y*narrow);
 				cellMesh.updateMatrix();
 				base.merge(cellGeom, cellMesh.matrix);
+				// if (x == 0 && y == 0) {
+				// 	bsp = new ThreeBSP(cellMesh);
+				// } else {
+				// 	bsp = bsp.union(new ThreeBSP(cellMesh));
+				// }
 				y++;
 			}
 			y = 0;
 			x++;
 		}
+		//finalGeom = bsp.toGeometry();
 		mesh = new THREE.Mesh(base, baseMaterial);
 		mesh.position.set((coords[0]*chunkWidth)+ (coords[2] % 2==0 ? 0 : chunkWidth / 2), (coords[1]*chunkWidth) / 2, coords[2]*chunkLength);
 
