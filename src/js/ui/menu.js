4d482fd2dffc90b904009737aec03e7e1126d388
Jeremy Evans <openspacehexagon@gmail.com>
2016-05-28 13:07:22 -0400

    Adjusted menu / dash UI

--- src/js/ui/menu.js
+++ src/js/ui/menu.js
@@ -66,7 +66,7 @@ export default class Menu extends React.Component {
 Menu.defaultProps = {
     name: 'main',
     options: [
-        {src: "/images/voxel.png", title: "Map View", text: "Activities", open: function(evt, menu) {
+        {src: "/images/voxel-white.png", title: "Map", text: "Activities", open: function(evt, menu) {
 			console.log("opening Launcher / activity view..");
 			menu.toggleActivityView();
 		} },
